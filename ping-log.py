b14d230df7133428f9e68161fa0581ada226e830
carloslpsouza <carlos.souza@unigranrio.br>
2023-06-25 21:09:49 -0300

    Organização dos arquivos correção do log 0 hora

--- ping-log.py
+++ ping-log.py
@@ -5,33 +5,12 @@ import atexit
 from time import sleep
 from datetime import datetime
 from assinatura import imprimeAssinatura
+from trataArquivo import *
 
 with open('config.json') as config_file:
     config_data = json.load(config_file)
 
 intervalo = config_data['intervalo']
-prefixo = config_data['prefixo_log']
-diretorio = config_data['diretorio-log']
-
-arquivos = os.listdir(diretorio)
-
-def le_arquivo(nome_arquivo, dir=""):
-    nome_arquivo = dir + nome_arquivo
-    with open(nome_arquivo, 'r') as arquivo:
-        linhas = arquivo.readlines()
-    return linhas
-
-
-def escreve_arquivo(nome_arquivo, conteudo):
-    nome_arquivo = diretorio + nome_arquivo
-    with open(nome_arquivo, 'w') as arquivo:
-        arquivo.writelines(conteudo)
-
-
-def atualiza_arquivo(nome_arquivo, conteudo):
-    nome_arquivo = diretorio + nome_arquivo
-    with open(nome_arquivo, 'a') as arquivo:
-        arquivo.writelines(conteudo)
 
 
 def limparTerminal():
@@ -51,12 +30,14 @@ def ping(host):
     return False
 
 
-def verificar_ips(arq_offline, arq_ips):
+def verificar_ips(arq_ips):
+    arq_offline = gera_nome_arquivo_log()
     if not os.path.isfile(os.path.join(diretorio, arq_offline)):
         escreve_arquivo(arq_offline, ['Start in: ' + str(datetime.now()) + '\n'])
-        print(1)
+        ips_offline = []
+    else:
+        ips_offline = le_arquivo(arq_offline, diretorio)
 
-    ips_offline = le_arquivo(arq_offline, diretorio)
     ips = le_arquivo(arq_ips)
 
     ips_offline_novos = []
@@ -67,21 +48,16 @@ def verificar_ips(arq_offline, arq_ips):
             limparTerminal()
             imprimeAssinatura()
             print('Dispositivo fora de rede! ' + str(data) + ' - ' + ip)
-
+            
     conteudo = ips_offline + ips_offline_novos
     escreve_arquivo(arq_offline, conteudo)
 
 
-def gera_nome_arquivo_log():
-    data = datetime.now().strftime('%Y-%m-%d')
-    for arquivo in arquivos:
-        if not str(data) == arquivo[-14:-4]:
-            return f'{prefixo}{data}.txt'
-    return f'{prefixo}{data}.txt'
-
 def finalizar_programa(arq_offline):
     if arq_offline is not None:
-        atualiza_arquivo(arq_offline, ['Finish in: ' + str(datetime.now()) + '\n'])
+        atualiza_arquivo(
+            arq_offline, ['Finish in: ' + str(datetime.now()) + '\n'])
+
 
 def main():
     arq_ips = 'ips'
@@ -90,13 +66,15 @@ def main():
     atexit.register(finalizar_programa, arq_offline)
 
     if not os.path.isfile(os.path.join(diretorio, arq_offline)):
-        escreve_arquivo(arq_offline, ['Start in: ' + str(datetime.now()) + '\n'])
+        escreve_arquivo(
+            arq_offline, ['Start in: ' + str(datetime.now()) + '\n'])
     else:
-        atualiza_arquivo(arq_offline, ['Start again in: ' + str(datetime.now()) + '\n'])
+        atualiza_arquivo(
+            arq_offline, ['Start again in: ' + str(datetime.now()) + '\n'])
 
     try:
         while True:
-            verificar_ips(arq_offline, arq_ips)
+            verificar_ips(arq_ips)
             sleep(intervalo)
     except KeyboardInterrupt:
         if arq_offline is not None:
---
+++ trataArquivo.py
@@ -0,0 +1,46 @@
+import os
+import json
+from datetime import datetime
+
+with open('config.json') as config_file:
+    config_data = json.load(config_file)
+
+prefixo   = config_data['prefixo_log']
+diretorio = config_data['diretorio-log']
+
+def le_arquivo(nome_arquivo, dir=""):
+    nome_arquivo = dir + nome_arquivo
+    with open(nome_arquivo, 'r') as arquivo:
+        linhas = arquivo.readlines()
+    return linhas
+
+def escreve_arquivo(nome_arquivo, conteudo):
+    nome_arquivo = diretorio + nome_arquivo
+    with open(nome_arquivo, 'w') as arquivo:
+        arquivo.writelines(conteudo)
+
+def atualiza_arquivo(nome_arquivo, conteudo):
+    nome_arquivo = diretorio + nome_arquivo
+    with open(nome_arquivo, 'a') as arquivo:
+        arquivo.writelines(conteudo)
+
+def trata_diretorio():
+  arquivos = os.listdir(diretorio)
+  return arquivos
+
+def gera_novo_arquivo(novo):
+  escreve_arquivo(
+            novo, ['Start in: ' + str(datetime.now()) + '\n'])
+  pass
+
+def gera_nome_arquivo_log():
+    data = datetime.now().strftime('%Y-%m-%d')
+    arquivos = trata_diretorio()
+    if len(arquivos) > 0:
+        for arquivo in arquivos:
+            if str(data) == arquivo[-14:-4]:
+              return str(arquivo)
+
+    gera_novo_arquivo(f'{prefixo}{data}.txt')
+    nome_arquivo = f'{prefixo}{data}.txt'
+    return nome_arquivo
